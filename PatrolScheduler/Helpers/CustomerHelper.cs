5affc465c0293656cf04dc4fe671738b23c25d46
Shaun Jordan <shaunjrdn@gmail.com>
2019-07-26 22:02:58 -0500

    Error validation

--- PatrolScheduler/Helpers/CustomerHelper.cs
+++ PatrolScheduler/Helpers/CustomerHelper.cs
@@ -1,16 +1,10 @@
 ﻿using PatrolScheduler.Database;
-using PatrolScheduler.ViewModel;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PatrolScheduler.Helpers
 {
-    public class CustomerHelper : BaseNotify, INotifyDataErrorInfo
+    public class CustomerHelper : ErrorValidation
     {
         public CustomerHelper(CapstoneCustomer customer)
         {
@@ -103,51 +97,6 @@ namespace PatrolScheduler.Helpers
                     }
                     break;
             }
-        }
-
-        private Dictionary<string, List<string>> _errorDictionary = new Dictionary<string, List<string>>();
-
-        public bool HasErrors => _errorDictionary.Any();
-
-        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
-
-        public IEnumerable GetErrors(string propertyName)
-        {
-            if (_errorDictionary.ContainsKey(propertyName))
-            {
-                return _errorDictionary[propertyName];
-            }
-            else
-            {
-                return null;
-            }
-        }
-
-        private void ErrorDictChanged(string propertyName)
-        {
-            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
-        }
-
-        private void AddErrors(string propertyName, string error)
-        {
-            if (!_errorDictionary.ContainsKey(propertyName))
-            {
-                _errorDictionary[propertyName] = new List<string>();
-            }
-            if (!_errorDictionary[propertyName].Contains(error))
-            {
-                _errorDictionary[propertyName].Add(error);
-                ErrorDictChanged(propertyName);
-            }
-        }
-
-        private void ClearErrors(string propertyName)
-        {
-            if (_errorDictionary.ContainsKey(propertyName))
-            {
-                _errorDictionary.Remove(propertyName);
-                ErrorDictChanged(propertyName);
-            }
-        }
+        }        
     }
 }
---
+++ PatrolScheduler/Helpers/ErrorValidation.cs
@@ -0,0 +1,57 @@
+﻿using PatrolScheduler.ViewModel;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace PatrolScheduler.Helpers
+{
+    public class ErrorValidation : BaseNotify, INotifyDataErrorInfo
+    {
+        private Dictionary<string, List<string>> _errorDictionary = new Dictionary<string, List<string>>();
+
+        public bool HasErrors => _errorDictionary.Any();
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            if (_errorDictionary.ContainsKey(propertyName))
+            {
+                return _errorDictionary[propertyName];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        protected virtual void ErrorDictChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        protected void AddErrors(string propertyName, string error)
+        {
+            if (!_errorDictionary.ContainsKey(propertyName))
+            {
+                _errorDictionary[propertyName] = new List<string>();
+            }
+            if (!_errorDictionary[propertyName].Contains(error))
+            {
+                _errorDictionary[propertyName].Add(error);
+                ErrorDictChanged(propertyName);
+            }
+        }
+
+        protected void ClearErrors(string propertyName)
+        {
+            if (_errorDictionary.ContainsKey(propertyName))
+            {
+                _errorDictionary.Remove(propertyName);
+                ErrorDictChanged(propertyName);
+            }
+        }
+    }
+}
